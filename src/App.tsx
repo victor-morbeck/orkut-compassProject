16b0254c366b0bf913b158546704127937f65e30
PedroHVM <pedromalheiros987@gmail.com>
2023-07-26 01:07:49 -0300

    adding some features to profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 
 
-function App() {
-  
+import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+const App: React.FC = () => {
   return (
-    <>
-     
-    </>
-  )
+    <div className="app">
+      <Header />
+      {/* Outras seções do LinkedIn seriam adicionadas aqui */}
+    </div>
+  );
 }
 
-export default App
+export default App;
