0459df647a6ac1e7e5e03fda14550cf699e1e1a5
Mike <mkeacs@gmail.com>
2023-07-26 14:48:31 -0300

    adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+import Login from '../src/components/Login/Login';
+import Profile from '../src/components/Profile/Profile';
+import Header from './components/Header/Header';
 
+const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-function App() {
-  
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <>
-     
-    </>
-  )
-}
+    <div className="App">
+      <Header isLoggedIn={isLoggedIn} />
+      {isLoggedIn ? <Profile /> : <Login onLoginSuccess={handleLoginSuccess} />}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à sua página de perfil!</h1>
+      {/* Restante do conteúdo da página de perfil */}
+    </div>
+  );
+};
+
+export default Profile;
