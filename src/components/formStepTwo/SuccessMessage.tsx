93913ed1348822556c8e0897da0d01a729815241
alicsal <larissa.alics@gmail.com>
2023-07-28 14:15:09 -0300

    formStepTwo
    new component representing the second page of the register form

---
+++ src/components/formStepTwo/SuccessMessage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuccessMessage: React.FC = () => {
+  return (
+    <div style={{ color: 'green', fontWeight: 'bold', fontSize: '18px' }}>
+      Cadastro realizado com sucesso!
+    </div>
+  );
+};
+
+export default SuccessMessage;
