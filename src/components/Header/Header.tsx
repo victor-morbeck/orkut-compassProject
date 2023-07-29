7af615a1456d55cb6d568798294982e097933a8b
Mike <mkeacs@gmail.com>
2023-07-29 03:44:23 -0300

    adding new features

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,37 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import Logo from "../../assets/image/logo-orkut.svg";
 
 interface HeaderProps {
-    isLoggedIn: boolean;
-  }
-  
-  const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
-    return (
-      <header className="header">
-        <nav>
-          <ul>
-            {isLoggedIn ? (
-              <>
-                <li>Início</li>
-                <li>Perfil</li>
-                <li>Comunidades</li>
-                <li>Jogos</li>
-              </>
-            ) : (
-              <>
-                <li>Sobre o Orkut</li>
-                <li>Centro de Segurança</li>
-              </>
-            )}
-          </ul>
-        </nav>
-      </header>
-    );
-  };
-  
-  export default Header;
+  isLoggedIn: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo">
+          <img src={Logo} alt="Logo Orkut" />
+        </div>
+        <ul>
+          {isLoggedIn ? (
+            <>
+              <li>Início</li>
+              <li>Perfil</li>
+              <li>Comunidades</li>
+              <li>Jogos</li>
+              <input className="header-input" type="text" placeholder="Pesquisar no Orkut" />
+            </>
+          ) : (
+            <>
+              <li>Sobre o Orkut</li>
+              <li>Centro de Segurança</li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
